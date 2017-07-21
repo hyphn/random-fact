1a95096f04fbd6f53af50ec26b0e769ed1ff8f23
Jakeoid <jake.triumphant@gmail.com>
2017-07-21 19:09:35 +1000

    Initial code commit.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# random-fact
-Grabs a random fact about the given category.
+# Random Birb.
+
+Welcome to the Random Birb repository, inside of here you can find a resource in which hosts the random images API. Inwhich none of the images of which Birb uses are hosted due to the fear of infringing on the terms of another individual.
+
+[If you want to checkout a live version of the API head to here.](https://random.birb.pw/)
+
+[This is a script that automatically renames files to smaller names.](https://gist.github.com/jakeoid/875c617dcc9fb1d67e493e5c65a645db)
+
+# Usage
+
+### 1. Make sure you have the required dependencies.
+
+```
+pip install -r ./requirements.txt
+```
+
+### 2. Rename `settings.template.json` to `settings.json` and edit contents.
+
+```
+nano settings.template.json >> settings.json
+```
+
+
+### 3. Run the script and head to the webpage in order to see it.
+
+```
+python3.5 app.py
+```
+
+With the standard install you should be able to click [here](http://127.0.0.1:5000/) and access it.
+
+# Contributing
+
+1. Fork repo.
+2. Edit code.
+3. Make a PR.
+4. Submit said PR.
+
+# License
+
+A copy of the MIT license can be found in `LICENSE.md`
---
+++ app.py
@@ -0,0 +1,67 @@
+"""
+
+RANDOM FACT SITE.
+
+:copyright: (c) 2017 Jakeoid.
+:license: MIT, see LICENSE.md for details.
+
+"""
+
+# KYOUKAI
+from kyoukai import Kyoukai
+from kyoukai import util
+
+# RANDOM
+from random import choice
+
+# HOUSEKEEPING
+import json
+
+# ############################
+
+# APP
+app = Kyoukai(__name__)
+
+# SETTINGS
+settings = None
+
+with open('settings.json') as settingsfile:
+    settings = json.load(settingsfile)
+
+# SERVER
+ip = settings['server']['ip']
+port = settings['server']['port']
+
+# ############################
+
+
+# Site Index.
+@app.route("/")
+async def index(ctx):
+    """The overall homepage/index of the website."""
+    file_directory = 'templates/index.html'
+
+    with open(file_directory) as file:
+        content = file.read()
+        return util.as_html(content)
+
+
+# Categories.
+@app.route("/api/v1/<name>")
+async def endpoint(ctx, name):
+    """The endpoint used in order to gather the facts about the categories."""
+
+    header = {
+        'Content-Type': 'application/json'
+    }
+
+    try:
+        return util.Response({"status": 200, "string": choice(settings['facts'][name])}, status=200, headers=header)
+    except:
+        return util.Response({"status": 404, "error": "That is not a valid category."}, status=200, headers=header)
+
+
+# ############################
+
+# Run our App.
+app.run_application(ip=ip, port=port)
